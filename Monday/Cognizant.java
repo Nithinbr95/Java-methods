e3ccb8ea9c6c52beb7601fcbc243b24995adc87f
nithinbr95 <nithin.xworkz@gmail.com>
2024-07-16 00:56:09 +0530

    15-07-24

---
+++ Monday/Cognizant.java
@@ -0,0 +1,24 @@
+public class Cognizant{
+
+public static String emp_name = "Vinay";
+public static byte emp_id = 29;
+public static short age = 25;
+public static int emp_sal = 45000;
+public static float weight = 40f;
+public static double height = 5.0;
+public static boolean worked = true;
+public static long emp_num = 9473846741L;
+public static char emp_gender = 'M';
+public static String location = "Bengalure";
+public static char blood_grp = 'O';
+public static int exp = 2;
+public static String education = "B.E";
+public static byte join_date = 24;
+public static short join_year = 2024;
+public static String join_month = "Octomber";
+public static String department = "Web Development";
+public Cognizant(){
+	
+	System.out.println("The Above information shows employee details");
+}
+}
